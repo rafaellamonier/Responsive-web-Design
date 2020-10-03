a92c117d8e620eb5125ad3998786c3a3b7aa9480
rafaellamonier <rafaellamonier17@hotmail.com>
2020-10-02 22:44:18 -0300

    first commit

---
+++ script/index.js
@@ -0,0 +1,6 @@
+function menuToggle() {
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
